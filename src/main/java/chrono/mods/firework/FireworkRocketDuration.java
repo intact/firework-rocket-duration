dc5cfea830e1d0b9403190f6cb6983da04dc267b
intact <intact.devel@gmail.com>
2021-06-08 18:07:04 +0200

    minecraft 1.17

--- src/main/java/chrono/mods/firework/FireworkRocketDuration.java
+++ src/main/java/chrono/mods/firework/FireworkRocketDuration.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
 
 import net.minecraft.item.Items;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.Identifier;
 
 public class FireworkRocketDuration implements ClientModInitializer {
@@ -13,8 +13,8 @@ public class FireworkRocketDuration implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		FabricModelPredicateProviderRegistry.register(Items.FIREWORK_ROCKET, new Identifier(MODID, "flight"),
-				(stack, world, entity) -> {
-					CompoundTag tags = stack.getSubTag("Fireworks");
+				(stack, world, entity, seed) -> {
+					NbtCompound tags = stack.getSubTag("Fireworks");
 					int flight = tags != null && tags.contains("Flight", 99) ? tags.getByte("Flight") : 1;
 					return Math.min(Math.max(flight - 1, 0), 2) / 2.0F;
 				});
