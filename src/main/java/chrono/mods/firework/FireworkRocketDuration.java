756e5062673d11a966f52141809d400534e925dd
intact <intact.devel@gmail.com>
2021-11-26 20:25:15 +0100

    use official Mojang mappings

--- src/main/java/chrono/mods/firework/FireworkRocketDuration.java
+++ src/main/java/chrono/mods/firework/FireworkRocketDuration.java
@@ -20,18 +20,18 @@ package chrono.mods.firework;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
 
-import net.minecraft.item.Items;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.util.Identifier;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Items;
 
 public class FireworkRocketDuration implements ClientModInitializer {
 	public static String MODID = "cr-firework-rocket-duration";
 
 	@Override
 	public void onInitializeClient() {
-		FabricModelPredicateProviderRegistry.register(Items.FIREWORK_ROCKET, new Identifier(MODID, "flight"),
+		FabricModelPredicateProviderRegistry.register(Items.FIREWORK_ROCKET, new ResourceLocation(MODID, "flight"),
 				(stack, world, entity, seed) -> {
-					NbtCompound tags = stack.getSubTag("Fireworks");
+					CompoundTag tags = stack.getTagElement("Fireworks");
 					int flight = tags != null && tags.contains("Flight", 99) ? tags.getByte("Flight") : 1;
 					return Math.min(Math.max(flight - 1, 0), 2) / 2.0F;
 				});
